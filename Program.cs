2fe6eefb4f1819a27b938c15f38fe23e3a27b00f
ali <alihcompiler@gmail.com>
2020-09-08 16:54:43 +0300

    fixing packaging issue

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AutoCrud
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("AutoCrud Library");
+        }
+    }
+}
